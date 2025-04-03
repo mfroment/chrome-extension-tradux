b0b16a9eb26ae23a02dce8d31d6d629468902169
Maxime Froment <mfroment99@gmail.com>
2025-04-03 16:32:10 +0900

    Select language, avoid excessive comments

--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
-  document.getElementById('apikey').addEventListener('input', (e) => {
-    chrome.storage.local.set({ openai_api_key: e.target.value });
-  });
-  
-  chrome.storage.local.get(["openai_api_key"], result => {
-    document.getElementById('apikey').value = result.openai_api_key || "";
-  });
+document.getElementById('apikey').addEventListener('input', (e) => {
+  chrome.storage.local.set({ openai_api_key: e.target.value });
+});
+
+document.getElementById('targetLang').addEventListener('input', (e) => {
+  chrome.storage.local.set({ target_lang: e.target.value });
+});
+
+chrome.storage.local.get(["openai_api_key", "target_lang"], result => {
+  document.getElementById('apikey').value = result.openai_api_key || "";
+  document.getElementById('targetLang').value = result.target_lang || "en";
+});
